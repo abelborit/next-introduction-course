5f4f9f9a25b8d09f15138a99a6f2d35f4ef20e40
Abel Borit Guitton <abelboritguitton@gmail.com>
2025-07-22 22:15:53 -0500

    [update] layouts

--- app/(wrapper)/layout.tsx
+++ app/(wrapper)/layout.tsx
@@ -1,8 +1,6 @@
 /* Como estructurar el proyecto en Next JS: https://nextjs.org/docs/app/getting-started/project-structure */
 /* ************************************************************************************************************************ */
 
-import { Navbar } from "@/app/components";
-
 export default function WrapperLayout({
   children,
 }: Readonly<{
@@ -10,8 +8,6 @@ export default function WrapperLayout({
 }>) {
   return (
     <>
-      <Navbar />
-
       <main className="flex flex-col items-center p-24 border-2 border-amber-500 m-2">
         <span className="text-lg">Wrapper Layout</span>
         {children}
